668c850bc1faed822391846326ffc20a9752b187
1qwaka <noemail>
2024-07-01 19:57:44 +0300

    refactor: small rename

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
-require("sp-utils");
-require("sp-info");
+require("sp-main");
 require("sp-subtitle");
 require("sp-schematics-loader");
 require("sp-load-schematics-dialog");
-require("sp-serializer");
-require("sp-constants");
 require("sp-globals");
 
 // REMINDER: 'const' in 'for of' loop makes syntax error
